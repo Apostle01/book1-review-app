f6b69c7d446f72a18d09b61ba8f7a8bfc9502824
Apostle01 <148454484+Apostle01@users.noreply.github.com>
2024-07-22 15:45:18 +0100

    Added 404.html file

--- app.py
+++ app.py
@@ -141,6 +141,10 @@ def book_details(book_id):
         flash('Comment added successfully', 'success')
     return render_template('book_details.html', book=book, form=form)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
