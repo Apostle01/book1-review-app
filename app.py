7187f7a9a03078a345865c9ed1430eaf85587ffa
Apostle01 <148454484+Apostle01@users.noreply.github.com>
2024-07-24 20:46:42 +0100

    Fix typo in app.run()

--- app.py
+++ app.py
@@ -173,4 +173,4 @@ def page_not_found(e):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, Debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
